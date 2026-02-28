0847c9560d4337b2288d7c3006a8e5085d43c6dd
BadOfficer <tarasbondarenko2005@gmail.com>
2026-02-28 21:23:18 +0200

    some changes

--- src/features/filters/api/filters.ts
+++ src/features/filters/api/filters.ts
@@ -2,12 +2,6 @@ import { instance } from '@/api/axios';
 import type { FiltersLimits } from '@/types/Filters';
 
 export async function getFiltersLimits(): Promise<FiltersLimits> {
-  // try {
-
-  // } catch (e) {
-  //   console.error(e);
-  //   throw new Error('Something went wrong');
-  // }
   const { data } = await instance.get<FiltersLimits>('/orders/filterlimits');
 
   return data;
